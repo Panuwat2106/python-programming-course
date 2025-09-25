bdd4f17fabf409b71fe2d0ec284b425a4b1565e6
Panuwat Rattanachot <panuwat.ratta@ku.th>
2025-09-25 15:22:55 +0700

    Week10

--- assignments/week10/personal-validator.py
+++ assignments/week10/personal-validator.py
@@ -28,3 +28,76 @@ Age Group: Young Adult (18-30)
 Phone: +91-9876543210
 
 """
+print("\n=== PERSONAL INFORMATION VALIDATOR ===")
+name = input("Enter your name:")
+age = int(input("Enter your age:"))
+phone = input("Enter your phone number:")
+
+print("\nValidation Results:")
+#name
+name_validator = True
+for char in name:
+  for char in name:
+   if char.isalpha() == True or char == " ":
+     name_validator = True
+   else:
+    name_validator = False
+    break
+
+#age
+age_validator = True
+if 18 <= age and age <=65 :
+ age_validator = True
+else:
+  age_validator = False
+
+#phone
+phone_validator = True
+if len(phone) != 10 :
+   phone_validator = False
+else :
+ for char in phone:
+  if char.isdigit() == False:
+     phone_validator = False
+     break
+
+#name
+if name_validator == True:
+  print("Name: Valid (contains only letters and spaces)")
+else :
+  print("Name: Invalid")
+
+#age
+if age_validator == True:
+  print(f"Age: Valid ({age} years old)")
+else :
+  print("Age: Invalid")
+
+#phone
+if phone_validator == True:
+  print("Phone: Valid (10-digit number)")
+else :
+  print("Phone: Invalid")
+  
+print("\nFormatted Information:")
+
+#name
+name = name.upper()
+print(f"Phone: {name}")
+
+#age
+if age >= 0 and age <= 12:
+    print("Age Group: Child (0-12)")
+elif age >= 13 and age <=17:
+    print("Age Group: Teenager (13-17)")
+elif age >= 18 and age <=30:
+    print("Age Group: Young Adult (18-30)")
+elif age >= 31 and age <=59:
+    print("Age Group: Adult (31-59)")
+elif age >= 60:
+    print("Age Group: Senior(>=60)")
+else:
+    print("Non")
+
+#phone
+print(f"Phone: +66-{phone}")
